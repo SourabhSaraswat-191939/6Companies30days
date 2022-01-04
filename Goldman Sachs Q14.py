74e2e234c0c9bae477d99f5b2d191d9b98edbdfd
Sourabh Saraswat <saraswatsourabh5@gmail.com>
2022-01-05 01:03:43 +0530

    Goldman Sachs Q14 Solved
    Signed-off-by: Sourabh Saraswat <saraswatsourabh5@gmail.com>

---
+++ Goldman Sachs Q14.py
@@ -0,0 +1,19 @@
+# Minimum Size Subarray Sum
+
+def minSubArrayLen(target, nums):
+    i=0
+    result = 0
+    length = len(nums)+1
+    for j in range(len(nums)):
+        result += nums[j]
+        while result>=target:
+                if length > j-i+1:
+                    length = j-i+1
+                    if length==1:
+                        return 1
+                result -= nums[i]
+                i+=1
+    
+    return length if length!=len(nums)+1 else 0
+
+print(minSubArrayLen(7,[2,3,1,2,4,3]))
